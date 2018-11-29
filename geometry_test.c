1f2c47dbb852217d44052b43a40157ab04ad51ec
Bridget Murphy <Bridget@engr2-19-7-dhcp.int.colorado.edu>
2018-11-29 10:33:30 -0700

    a,jhfb

--- geometry_test.c
+++ geometry_test.c
@@ -157,7 +157,7 @@ START_TEST(test_2d_area_triangle){
 	c.x = 3;
 	c.y = 4;
 
-	ck_assert(coord_2d_area_triangle(&a,&b,&c)=3);
+	ck_assert(coord_2d_area_triangle(&a,&b,&c)==3);
 
         coord_2d a;
         coord_2d b;
@@ -170,7 +170,7 @@ START_TEST(test_2d_area_triangle){
         c.x = 3;
         c.y = 4;
 
-        ck_assert(coord_2d_area_triangle(&a,&b,&c)=6);
+        ck_assert(coord_2d_area_triangle(&a,&b,&c)==6);
 }
 END_TEST
 
